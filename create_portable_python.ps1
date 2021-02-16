18e6cc11dd365f47311d9075eccacd71620bcc1b
mortenee <mortenee@gmail.com>
2021-02-16 14:21:15 +0100

    pip proxy workaround

--- create_portable_python.ps1
+++ create_portable_python.ps1
@@ -23,22 +23,23 @@ If (Test-Path "$tmpDir\$pythonEmbeddedZipName") {
     }
 }
 
-# Add pip and tkinter to embedded python if extracted embedded python exists:
-if(-Not (Test-Path "$pythonEmbeddedZipExtractedPath\get-pip.py")) {
-    $url = "https://bootstrap.pypa.io/get-pip.py"
-    $output = "$pythonEmbeddedZipExtractedPath\get-pip.py"
-    Invoke-WebRequest -Uri $url -OutFile $output
-    & $pythonEmbeddedZipExtractedPath\python.exe $pythonEmbeddedZipExtractedPath\get-pip.py 'pip==20.2.4' # SSL error on later
-    # Fix python path:
-    $pthFile = "$pythonEmbeddedZipExtractedPath\python$pythonInstalledMajorVersion._pth"
-    $text = [string]::Join("`n", (Get-Content $pthFile))
-    [regex]::Replace($text, "\.`n", ".`nLib\site-packages`n..\..\..\`n", "Singleline") | Set-Content $pthFile
-    # Copy Tkinter to embedded from installed:
-    Copy-Item -Path "$pythonInstalledDir\tcl" -Destination $pythonEmbeddedZipExtractedPath -recurse -Force
-    Copy-Item -Path "$pythonInstalledDir\Lib\tkinter" -Destination $pythonEmbeddedZipExtractedPath -recurse -Force
-    Copy-Item -Path "$pythonInstalledDir\DLLs\_tkinter.pyd" -Destination $pythonEmbeddedZipExtractedPath -Force
-    Copy-Item -Path "$pythonInstalledDir\DLLs\tcl86t.dll" -Destination $pythonEmbeddedZipExtractedPath -Force
-    Copy-Item -Path "$pythonInstalledDir\DLLs\tk86t.dll" -Destination $pythonEmbeddedZipExtractedPath -Force
-    $pipProcess = Start-Process -NoNewWindow -FilePath "$pythonEmbeddedZipExtractedPath\python.exe" -ArgumentList "-m pip install --upgrade --no-warn-script-location --force-reinstall JPype1 blake3 psutil jaydebeapi toposort flake8 autopep8 rope beautifulsoup4 lxml pygments petl filetype -t $pythonEmbeddedZipExtractedPath\Lib\site-packages" -PassThru; $pipProcess.WaitForExit()
-    Compress-Archive -Path $pythonEmbeddedZipExtractedPath\* -DestinationPath "$pythonInstalledParentDir\python-$pythonInstalledVersion-embed-amd64.zip" -Force
-}
+# Add pip and tkinter to embedded python:
+$url = "https://github.com/Preservation-Workbench/windows_deps/releases/latest/download/get-pip.py"
+$output = "$pythonEmbeddedZipExtractedPath\get-pip.py"
+Invoke-WebRequest -Uri $url -OutFile $output
+& $pythonEmbeddedZipExtractedPath\python.exe $pythonEmbeddedZipExtractedPath\get-pip.py --proxy='http://proxy.srv.bergenkom.no:8080' 'pip==20.2.4' # SSL error on later
+#& $pythonEmbeddedZipExtractedPath\python.exe $pythonEmbeddedZipExtractedPath\get-pip.py 'pip==20.2.4' # SSL error on later
+# Fix python path:
+$pthFile = "$pythonEmbeddedZipExtractedPath\python$pythonInstalledMajorVersion._pth"
+$text = [string]::Join("`n", (Get-Content $pthFile))
+[regex]::Replace($text, "\.`n", ".`nLib\site-packages`n..\..\..\`n", "Singleline") | Set-Content $pthFile
+# Copy Tkinter to embedded from installed:
+Copy-Item -Path "$pythonInstalledDir\tcl" -Destination $pythonEmbeddedZipExtractedPath -recurse -Force
+Copy-Item -Path "$pythonInstalledDir\Lib\tkinter" -Destination $pythonEmbeddedZipExtractedPath -recurse -Force
+Copy-Item -Path "$pythonInstalledDir\DLLs\_tkinter.pyd" -Destination $pythonEmbeddedZipExtractedPath -Force
+Copy-Item -Path "$pythonInstalledDir\DLLs\tcl86t.dll" -Destination $pythonEmbeddedZipExtractedPath -Force
+Copy-Item -Path "$pythonInstalledDir\DLLs\tk86t.dll" -Destination $pythonEmbeddedZipExtractedPath -Force
+$pipProcess = Start-Process -NoNewWindow -FilePath "$pythonEmbeddedZipExtractedPath\python.exe" -ArgumentList "-m pip install --upgrade --no-warn-script-location --force-reinstall JPype1 blake3 psutil jaydebeapi toposort flake8 autopep8 rope beautifulsoup4 lxml pygments petl filetype -t $pythonEmbeddedZipExtractedPath\Lib\site-packages" -PassThru; $pipProcess.WaitForExit()
+Compress-Archive -Path $pythonEmbeddedZipExtractedPath\* -DestinationPath "$pythonInstalledParentDir\python-$pythonInstalledVersion-embed-amd64.zip" -Force
+
+
