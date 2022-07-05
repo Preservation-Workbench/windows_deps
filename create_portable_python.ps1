997ca3357ba95855c935b1136c0fa4187da83c93
mortenee <mortenee@gmail.com>
2022-07-05 13:08:52 +0200

    Add specific_import

--- create_portable_python.ps1
+++ create_portable_python.ps1
@@ -38,6 +38,6 @@ Copy-Item -Path "$pythonInstalledDir\Lib\tkinter" -Destination $pythonEmbeddedZi
 Copy-Item -Path "$pythonInstalledDir\DLLs\_tkinter.pyd" -Destination $pythonEmbeddedZipExtractedPath -Force
 Copy-Item -Path "$pythonInstalledDir\DLLs\tcl86t.dll" -Destination $pythonEmbeddedZipExtractedPath -Force
 Copy-Item -Path "$pythonInstalledDir\DLLs\tk86t.dll" -Destination $pythonEmbeddedZipExtractedPath -Force
-$pipProcess = Start-Process -NoNewWindow -FilePath "$pythonEmbeddedZipExtractedPath\python.exe" -ArgumentList "-m pip install --upgrade --no-warn-script-location --force-reinstall JPype1 blake3 psutil jaydebeapi toposort flake8 autopep8 rope beautifulsoup4 lxml pygments petl filetype -t $pythonEmbeddedZipExtractedPath\Lib\site-packages" -PassThru; $pipProcess.WaitForExit()
+$pipProcess = Start-Process -NoNewWindow -FilePath "$pythonEmbeddedZipExtractedPath\python.exe" -ArgumentList "-m pip install --upgrade --no-warn-script-location --force-reinstall JPype1 blake3 psutil jaydebeapi toposort specific_import flake8 autopep8 rope beautifulsoup4 lxml pygments petl filetype -t $pythonEmbeddedZipExtractedPath\Lib\site-packages" -PassThru; $pipProcess.WaitForExit()
 Compress-Archive -Path $pythonEmbeddedZipExtractedPath\* -DestinationPath "$pythonInstalledParentDir\python-$pythonInstalledVersion-embed-amd64.zip" -Force
 
